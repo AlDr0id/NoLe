77e2635b89e76c1a750bf6a0d1b02fff15dfd69e
jpicatos <32961669+jpicatos@users.noreply.github.com>
2018-05-15 01:09:01 +0200

    Add files via upload

--- product-display.php
+++ product-display.php
@@ -40,15 +40,46 @@
 			    $path = 'product.php?id='.$prod[$i]->getId().'';
 			    echo"<h1>".$prod[$i]->getNombre()."</h1>"; ?>
 			    <div class="author">
-			    	<img src="pica.jpg"/>
+			    	<img src="pica.jpg"/> <!-- Imagen que habra que cambiar cuando se tengan fotos del usuario -->
 			      	<h2><?php echo $prod[$i]->getOwner() ?></h2>
 			    </div>
 			    <div class="category">
-			      	<h2><?php echo $prod[$i]->getCategoria() ?></h2>
+			      	<?php 
+			      		switch ($prod[$i]->getCategoria()) {
+			      		 	case '0':
+			      		 		echo "<a class='catLink' href='numismatica.php'> Numismática</a>";
+			      		 		break;
+			      		 	case '1':
+			      		 		echo "<a>Rincón de la Abuela</a>";
+			      		 		break;
+			      		 	case '2':
+			      		 		echo "<a>Figuras</a>";
+			      		 		break;
+			      		 	case '3':
+			      		 		echo "<a>Filatelia</a>";
+			      		 		break;
+			      		 	case '4':
+			      		 		echo "<a>Vinilos/Discos</a>";
+			      		 		break;
+			      		 	case '5':
+			      		 		echo "<a>Cromos</a>";
+			      		 		break;
+			      		 	case '6':
+			      		 		echo "<a>Libros/Comics</a>";
+			      		 		break;
+			      		 	case '7':
+			      		 		echo "<a>Trastero</a>";
+			      		 		break;
+			      		 }?>
+			      	
 			    </div>
+			    <div class="precio">
+			      	<h2><?php echo $prod[$i]->getPrecio() ?>$</h2>
+			    </div>
+
 			    <div class="separator"></div>
 			    <p><?php echo $prod[$i]->getDescripcionCorta() ?></p>
-			    <?php echo '<a class="seemore" href='.$path.'><i class="right"></i><p>Ir al producto</p></a>'?>
+			    <?php echo '<a class="seemore" href='.$path.'><p>Ir al producto</p></a>'?>
 
 		  </div>
 		</div>
