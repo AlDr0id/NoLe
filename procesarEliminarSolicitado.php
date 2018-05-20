71a93ab855a45ab8a312c090aeedbf10fe4b1f93
alberto402 <39036564+alberto402@users.noreply.github.com>
2018-05-20 15:44:10 +0200

    PRODUCTO SOLICITADO CON INTERFAZ FEA

---
+++ procesarEliminarSolicitado.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once("include/productoSolicitadoSA.php");
+require_once("include/productoSolicitadoTransfer.php");
+
+$data = array();
+
+function eliminaCuenta( $id){
+ 	$productoSa = new productoSolicitadoSA();
+ 	$correcto=$productoSa->eliminar($id);
+ 	return $correcto;
+}
+
+$id = htmlspecialchars(trim(strip_tags($_GET["id"])));
+$anadido = eliminaCuenta($id);
+ if($anadido != NULL){
+
+      $data['success'] = True;
+
+      echo"<script language='JavaScript'>
+              alert('Busqueda Borrada');
+         </script>";
+      header("Refresh: 0 ;URL= perfil.php");
+
+  }
+  else {
+      $data['success'] = False;
+      $data['errors'] = 'No se ha podido eliminar la Busqueda';
+  }
+echo json_encode($data);
+?>
--- verProductosSolicitados.php
+++ verProductosSolicitados.php
@@ -12,18 +12,39 @@
 		<div class="card">
 			<div class="details">
 				<?php
+						
 					$perfil = 'perfilVisitante.php?nickname='.$prod[$i]->getId_user().'';
 					echo"<h1>".$prod[$i]->getNombreP()."</h1>"; ?>
 					<div class="author">
-						<?php echo '<a class ="seemore" href='. $perfil . '></i><img src="pica.jpg"/>
+						<?php echo '<a class ="seemore" href='. $perfil . '></i><img src=\"img/"tito.png\"/>
 			              <h2>'. $prod[$i]->getId_user() .'</h2></a>' ?>
 					</div>
 					<div class="category">
 						<h2><?php echo $prod[$i]->getCategoria() ?></h2>
 					</div>
+					<?php
+					if( $prod[$i]->getId_Producto()!= NULL){
+
+					?>
+							<div class="producto">
+								<?php echo '<a class ="catLink" href=product.php?id='. $prod[$i]->getId_Producto() . '>
+					              <h2>'."Ir al producto".'</h2></a>' ?>
+							
+							</div>
+					<?php
+						}
+					?>
+
+					<div class="imagen">
+					<?php echo '<div class="thumbnail"><img class="left-prod" height="200" width="200" src="img/'.$prod[$i]->getId_Producto().'.png"/></div>'; ?>
+					</div>
 					<div class="separator"></div>
 					<p>Palabras clave: <?php echo $prod[$i]->getPalabrasClave() ?></p>
-				?>
+					<div class="eliminar">
+						<?php echo '<a class ="catLink" href=procesarEliminarSolicitado.php?id='. $prod[$i]->getId() .'&idP='. $prod[$i]->getId_Producto() .'> 
+			              <h2>'."Eliminar".'</h2></a>' ?>
+					</div>
+				
 
 			</div>
 		</div>
