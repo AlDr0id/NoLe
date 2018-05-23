e8ddf3901dbe1f75e70a2109648be96c31577c29
Rodri <39036536+rodrigolagartera@users.noreply.github.com>
2018-05-23 11:08:37 +0200

    Vista verProductoSolicitados

--- verProductosSolicitados.php
+++ verProductosSolicitados.php
@@ -15,36 +15,66 @@
 						
 					$perfil = 'perfilVisitante.php?nickname='.$prod[$i]->getId_user().'';
 					echo"<h1>".$prod[$i]->getNombreP()."</h1>"; ?>
-					<div class="author">
-						<?php echo '<a class ="seemore" href='. $perfil . '></i><img src=\"img/"tito.png\"/>
-			              <h2>'. $prod[$i]->getId_user() .'</h2></a>' ?>
-					</div>
-					<div class="category">
-						<h2><?php echo $prod[$i]->getCategoria() ?></h2>
-					</div>
 					<?php
 					if( $prod[$i]->getId_Producto()!= NULL){
 
 					?>
-							<div class="producto">
-								<?php echo '<a class ="catLink" href=product.php?id='. $prod[$i]->getId_Producto() . '>
-					              <h2>'."Ir al producto".'</h2></a>' ?>
-							
-							</div>
-
 							<div class="imagen">
 							<?php echo '<div class="thumbnail"><img class="left-prod" height="200" width="200" src="img/'.$prod[$i]->getId_Producto().'.png"/></div>'; ?>
+							</div>
+
+							<div class="producto">
+								<?php echo '<a class ="seemore" href=product.php?id='. $prod[$i]->getId_Producto() . '>
+					              <p>'."Ir al producto".'</p></a>' ?>
+							
 							</div>
 					<?php
 						}
 					?>
+					<div class="author">
+						<?php echo '<a class ="seemore" href='. $perfil . '><img src="img/'. $prod[$i]->getId_user() .'.png"/>
+			              <h2>'. $prod[$i]->getId_user() .'</h2></a>' ?>
+					</div>
+					<div class="category">
+						<?php
+						switch ($prod[$i]->getCategoria()) {
+							case '0':
+								echo "<a class='catLink' href='numismatica.php'>Numistamica</a>";
+								break;
+							case '1':
+								echo "<a class='catLink' href='rinconAbuela.php'>Rincón de la Abuela</a>";
+								break;
+							case '2':
+								echo "<a class='catLink' href='figuras.php'>Figuras</a>";
+								break;
+							case '3':
+								echo "<a class='catLink' href='filatelia.php'>Filatelia</a>";
+								break;
+							case '4':
+								echo "<a class='catLink' href='vinilosDiscos.php'>Vinilos/Discos</a>";
+								break;
+							case '5':
+								echo "<a class='catLink' href='cromos.php'>Cromos</a>";
+								break;
+							case '6':
+								echo "<a class='catLink' href='librosComics.php'>Libros/Comics</a>";
+								break;
+							case '7':
+								echo "<a class='catLink' href='trastero.php'>Trastero</a>";
+								break;
 
+							default:
+								echo "Pendiente";
+								break;
+						}
+						?>
+					</div>
 					
 					<div class="separator"></div>
 					<p>Palabras clave: <?php echo $prod[$i]->getPalabrasClave() ?></p>
 					<div class="eliminar">
-						<?php echo '<a class ="catLink" href=procesarEliminarSolicitado.php?id='. $prod[$i]->getId() .'&idP='. $prod[$i]->getId_Producto() .'> 
-			              <h2>'."Eliminar".'</h2></a>' ?>
+						<?php echo '<a class ="seemore" href=procesarEliminarSolicitado.php?id='. $prod[$i]->getId() .'&idP='. $prod[$i]->getId_Producto() .'> 
+			              <p>'."Eliminar".'</p></a>' ?>
 					</div>
 				
 
